923e0c317c15d766e3165916e0f319b8bffa0f14
Pavel <leniviy.putnik@gmail.com>
2019-07-18 16:46:10 +0400

    Домашка 3

--- services/IBD.php
+++ services/IBD.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\services;
 
+use App\models\Model;
+
 interface IBD
 {
-    public function find(string $sql);
-    public function findAll(string $sql);
+    public function find(Model $obj, string $sql, array $params = [] );
+    public function findAll(Model $obj, string $sql, array $params = []);
 }
