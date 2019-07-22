bd8011d6cdeb28b9d0e9cd3ec30235431711d1c7
Pavel <leniviy.putnik@gmail.com>
2019-07-22 15:44:21 +0400

    Домашка 4

--- models/Model.php
+++ models/Model.php
@@ -22,7 +22,7 @@ abstract class Model
      * Данный метод должен вернуть название таблицы
      * @return string
      */
-    abstract protected function getTableName();
+    abstract protected static function getTableName();
 
     /**
      * Возращает пользователя с указанным id
@@ -30,35 +30,90 @@ abstract class Model
      * @param int $id ID Записи таблицы
      * @return array
      */
-    public function getOne($id)
+    public static function getOne($id)
     {
-        $tableName = $this->getTableName();
+        $tableName = static::getTableName();
         $sql = "SELECT * FROM {$tableName} WHERE id = :id";
-        return $this->bd->find($this, $sql, [':id' => $id]);
+        return BD::getInstance()->find(
+            get_called_class(),
+            $sql,
+            [':id' => $id]);
+
 }
 
     /**
      *
      */
-    public function getAll()
+    public static function getAll()
     {
-        $tableName = $this->getTableName();
+        $tableName = static::getTableName();
         $sql = "SELECT * FROM {$tableName}";
-        return $this->bd->findAll($this, $sql);
-    }
-
-    public function save()
-    {
-        $this->bd->save($this, $this->getTableName());
+        return BD::getInstance()->findAll(get_called_class(), $sql);
     }
 
     public function delete()
     {
-        $this->bd->delete($this, $this->getTableName());
+        $table = static::getTableName();
+        $sql = "DELETE FROM {$table} WHERE id = :id";
+        $this->bd->execute($sql, [':id' => $this->id]);
     }
 
     public function test()
     {
-        $this->bd->testAdd($this);
+        $names = "";
+        $values = "";
+        foreach ($this as $name => $value ){
+            if($name != 'id') {
+                $names .= $name.", ";
+                $values.= "'{$value}', ";}
+        }
+        $names  = substr($names, 0, -2);
+        $values  = substr($values, 0, -2);
+        $sql = "INSERT INTO users ({$names}) VALUES ({$values})";
+        $this->bd->execute($sql);
+    }
+
+    public function save()
+    {
+        $table = static::getTableName();
+        $temp = $this->bd->execute("SELECT * FROM {$table} WHERE name = :name", [':name'=>$this->name]);
+        $temp->setFetchMode(\PDO::FETCH_CLASS, get_called_class());
+        if($temp->fetch())
+        {
+            $this->update($this, $table);
+            //          echo "updating";
+
+        }else {
+            $this->insert($this, $table);
+            //          echo "inserting";
+        }
+
+    }
+
+    private function update($obj, $table)
+    {
+        $string = '';
+        foreach ($obj as $name => $value ){
+            if($name != 'id' && $name!='bd') {
+                $string .= "{$name}='{$value}', ";
+            }
+        }
+        $string  = substr($string, 0, -2);
+        $sql = "UPDATE {$table} SET {$string} WHERE id = {$obj->id}";
+        $this->bd->execute($sql);
+    }
+    private function insert($obj, $table)
+    {
+        $names = "";
+        $values = "";
+        foreach ($obj as $name => $value ){
+            if($name != 'id' && $name!='bd') {
+                $names .= $name.", ";
+                $values.= "'{$value}', ";}
+        }
+        $names  = substr($names, 0, -2);
+        $values  = substr($values, 0, -2);
+        $sql = "INSERT INTO {$table} ({$names}) VALUES ({$values})";
+        $this->bd->execute($sql);
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,26 +2,19 @@
 
 include '../services/Autoload.php';
 
-use App\models\User;
-use App\services\BD;
-
 spl_autoload_register(
     [new App\services\Autoload(),
         'loadClass']
 );
 
-$user = new User();
-$user->getOne(1);
-//var_dump( $user);
+$controllerName = $_REQUEST['c'] ?: 'good';
+$actionName = $_REQUEST['a'];
 
-$users = $user->getAll();
-
-$user2 = new User();
-$user2->getOne(5);
-$user2->login = 'RGFIGsa2323';
-
-
-$user2->save();
-//include 'form.html';
+$controllerClass = 'App\\controllers\\' .
+    ucfirst($controllerName) . 'Controller';
+if (class_exists($controllerClass)) {
+    $controller = new $controllerClass();
+    $controller->run($actionName);
+}
 
 
